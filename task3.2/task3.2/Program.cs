084619cbd47d8939e7cd7b158e59a9b88059e9f9
gelemani <fedorkrutikov7352@gmail.com>
2024-02-05 23:39:21 +0300

    sdls

---
+++ task3.2/task3.2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    public static void main(string[] args)
+    {
+        Cat cat = new Cat();    
+        cat.ID = 1;
+        cat.Price();
+    }
+}
---
+++ task3.2/task3.2/jag.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Cat
+{
+    public int Id { get; set; }
+
+    public void Price ()
+    {
+        Console.WriteLine(Id);
+    }
+}
