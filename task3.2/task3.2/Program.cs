11a413c1dce9bd1fed863d32b284faad302a6824
gelemani <88558485+gelemani@users.noreply.github.com>
2024-02-11 22:52:41 +0300

    Update Program.cs

--- task3.2/task3.2/Program.cs
+++ task3.2/task3.2/Program.cs
@@ -1,45 +1,17 @@
-﻿using System;
+using System;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine ("\nOne-dimensional");
-        Console.WriteLine("Enter array's length: ");
-        UnoDimensional uno = new UnoDimensional(int.Parse(Console.ReadLine()));
-        uno.Create();
-        uno.Print();
-        uno.MiddleValue();
-
-
-        Console.WriteLine("\nTwo-dimensional");
-        (int, int) dosInput = Input();
-        DosDimensional dos = new DosDimensional(dosInput.Item1, dosInput.Item2);
-        dos.Create();
-        dos.Print();
-        dos.MiddleValue();
-
-
-        Console.WriteLine("\nJagged array");
-        JaggedDimensional jag = new JaggedDimensional(InputJaggedDimensional());
-        jag.Create();
-        jag.Print();
-        jag.MiddleValue();
-    }
-
-    static (int, int) Input()
-    {
-        Console.WriteLine("Enter number of rows: ");
-        int rows = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter number of columns: ");
-        int columns = int.Parse(Console.ReadLine());
-        return (rows, columns);
-    }
-
-    static int InputJaggedDimensional()
-    {
-        Console.WriteLine("Enter number of rows: ");
-        int rows = int.Parse(Console.ReadLine());
-        return rows;
+        ArrayBase[] ab = new ArrayBase[3];
+        ab[0] = new UnoDimensional(5);
+        ab[1] = new DosDimensional(4, 4);
+        ab[3] = new JaggedDimensional(3);
+        foreach (var item in ab)
+        { 
+            item.MiddleValue();
+            item.Print();
+        }
     }
 }
