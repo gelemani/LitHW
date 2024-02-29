e9448ac94e837996cec54a21e21afc3f45a0fcbc
gelemani <fedorkrutikov7352@gmail.com>
2024-02-29 22:09:36 +0300

    3.4 add T

--- task3.4/Program.cs
+++ task3.4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayBase[] ab = new ArrayBase[3];
+        ab[0] = new UnoDimensional(5);
+        ab[1] = new DosDimensional(4, 4);
+        ab[2] = new JaggedDimensional(3);
+        foreach (var item in ab)
+        {
+            item.Print();
+            item.MiddleValue();
+        }
+        Console.WriteLine("\n-----------------------------------------------------------------------");   
+        var week = new Weekdays();
+        IUnoDimensional unoDim = (IUnoDimensional)ab[0];
+        unoDim.DeleteDuplicates();  
+        IDosDimensional dosDim = (IDosDimensional)ab[1];
+        IJaggedDimensional jagDim = (IJaggedDimensional)ab[2];
+        jagDim.ChangeChet();
+
+        IPrinter[] pr = { unoDim, dosDim, jagDim, week };
+        foreach (var item in pr)
+        {
+            item.Print();
+        }
+    }
+}
