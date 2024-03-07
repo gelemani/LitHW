8d4174696410c72fe674f0ef157f0cc85ec6e12b
gelemani <88558485+gelemani@users.noreply.github.com>
2024-03-07 12:17:13 +0300

    Update uno.cs

--- task3.4/uno.cs
+++ task3.4/uno.cs
@@ -6,14 +6,32 @@ public sealed class UnoDimensional<T> : ArrayBase
 {
     private T[] _array;
     private bool choice;
+    private IArrayFill<T> _fill;
 
-    public UnoDimensional(int length, T fill)
+    public UnoDimensional(int length, IArrayFill<T> fill)
     {
         _array = new T[length];
         IArrayFill(length);
-
-
+        _fill = fill;
     }
+
+    protected override void RandFill()
+    {
+        Random rand = new Random();
+        for (int i = 0; i < _array.Length; i++)
+        {
+            _array[i] = _fill.GetRandomValue();
+        }
+    }
+
+    protected override void HandFill()
+    {
+        for (int i = 0; i < _array.Length; i++)
+        {
+            _array[i] = _fill.GetUserValue();
+        }
+    }
+
 
     public override void Print()
     {
