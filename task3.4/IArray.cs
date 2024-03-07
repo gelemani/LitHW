2431a77c8536bd9cd5696002842d7aacbfaa36a3
gelemani <88558485+gelemani@users.noreply.github.com>
2024-03-07 12:25:00 +0300

    Update IArray.cs

--- task3.4/IArray.cs
+++ task3.4/IArray.cs
@@ -1,6 +1,4 @@
-internal interface IArray<T> : IPrinter
+public interface IArray<T> : IPrinter
 {
-    int Length { get; }
-    T this[int index] { get; }
-    void ReCreate(int length);
+    void Create();
 }
