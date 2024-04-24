05997f68b1873434073c54b07813caf096ed8ad6
gelemani <88558485+gelemani@users.noreply.github.com>
2024-04-24 10:37:54 +0300

    Create app

---
+++ task4.3/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace task4._3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
