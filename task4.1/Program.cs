bece249c51e9d7425ec53db0da16330bd3c48134
gelemani <fedorkrutikov7352@gmail.com>
2024-05-08 21:55:41 +0300

    done

--- task4.1/Program.cs
+++ task4.1/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.Collections;
 namespace task4._1;
@@ -123,4 +124,132 @@ class Program
         unoFloat.iterForeach((x) => Console.Write(x * x * x + " "));
         Console.WriteLine("\n");
     }
+=======
+using System;
+using System.Collections;
+namespace task4._1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("-----------------------int-----------------------");
+        UnoDimensional<int> unoInt = new(5);
+
+        unoInt.Add(1);
+        unoInt.Add(215);
+        unoInt.Add(-334);
+        unoInt.Add(40);
+        unoInt.Add(-50);
+        unoInt.Print();
+
+        Console.Write("\nSorted - ");
+        unoInt.Sorting();
+        unoInt.Print();
+
+        Console.Write("\nReversed - ");
+        unoInt.Reverse();
+        unoInt.Print();
+        
+        Func<int, int, int> compare = (x, y) => x.CompareTo(y);
+        Console.WriteLine($"\nMin {unoInt.FindMin<int>(compare)}");
+        Console.WriteLine($"Max {unoInt.FindMax<int>(compare)}");
+
+        Console.Write("\nElement removed - ");
+        unoInt.Remove(4);
+        unoInt.Print();
+
+        Console.WriteLine();    
+        unoInt.ForEachAction<int>((x) =>
+        {
+            double pow = Math.Pow(x, 2);
+            Console.WriteLine("Value = " + x + ", pow^2 = " + pow);
+        });
+
+        Console.WriteLine($"\nAll elementes - {unoInt.CountArray<int>()}");
+
+        Console.WriteLine($"With condition - {unoInt.CountArrayWithCondition<int>((x) => x % 2 == 1)}");
+
+        Console.WriteLine($"With at least one  - {unoInt.EvenOneCondition<int>((x) => {
+            if (x > 50) { return true; }
+            else { return false; }
+        })}");
+
+        Console.WriteLine($"With all - {unoInt.AllCondition<int>((x) => {
+            if (x >= 0) { return true; }
+            else { return false; }
+        })}");
+
+        Console.WriteLine($"\nContains - {unoInt.Contains<int>(215)}");
+
+        int[] numbersInt = unoInt.Where<int>((x) => x < 100);
+        Console.WriteLine($"Array with condition - {string.Join(' ', numbersInt)}");
+
+        Console.WriteLine($"\nArray from index - {unoInt.CountArrayFromIndex<int>(1)}");
+
+        Console.Write("\nForeach - ");
+        unoInt.iterForeach((x) => Console.Write(x * x * x + " "));
+
+
+        Console.WriteLine("\n\n\n-----------------------float-----------------------");
+        UnoDimensional<float> unoFloat = new(4);
+
+        unoFloat.Add(5.1f);
+        unoFloat.Add(100.500f);
+        unoFloat.Add(33f);
+        unoFloat.Add(-500.17f);
+        unoFloat.Print();
+
+        Console.Write("\nSorted - ");
+        unoFloat.Sorting();
+        unoFloat.Print();
+
+        Console.Write("\nReversed - ");
+        unoFloat.Reverse();
+        unoFloat.Print();
+
+        Func<float, float, int> compareFloat = (x, y) => x.CompareTo(y);
+        Console.WriteLine($"\nMin {unoFloat.FindMin<float>(compareFloat)}");
+        Console.WriteLine($"Max {unoFloat.FindMax<float>(compareFloat)}");
+
+        Console.Write("\nElement removed - ");
+        unoFloat.Remove(8);
+        unoFloat.Print();
+
+        Console.WriteLine();
+        unoFloat.ForEachAction<float>((x) =>
+        {
+            double pow = Math.Pow(x, 2);
+            Console.WriteLine("Value = " + x + ", pow^2 = " + pow);
+        });
+
+        Console.WriteLine($"\nAll elementes - {unoFloat.CountArray<float>()}");
+
+        Console.WriteLine($"With condition - {unoFloat.CountArrayWithCondition<float>((x) => x % 2 == 1)}");
+
+        Console.WriteLine($"With at least one  - {unoFloat.EvenOneCondition<float>((x) =>
+        {
+            if (x > 50) { return true; }
+            else { return false; }
+        })}");
+
+        Console.WriteLine($"With all - {unoFloat.AllCondition<float>((x) =>
+        {
+            if (x >= 0) { return true; }
+            else { return false; }
+        })}");
+
+        Console.WriteLine($"\nContains - {unoFloat.Contains<float>(21.5f)}");
+
+        float[] numbersFloat = unoFloat.Where<float>((x) => x < 100f);
+        Console.WriteLine($"Array with condition - {string.Join(' ', numbersFloat)}");
+
+        Console.WriteLine($"\nArray from index - {unoFloat.CountArrayFromIndex<float>(1)}");
+
+        Console.Write("\nForeach - ");
+        unoFloat.iterForeach((x) => Console.Write(x * x * x + " "));
+        Console.WriteLine("\n");
+
+    }
+>>>>>>> 9908cc8 (done!)
 }
--- task4.1/uno.cs
+++ task4.1/uno.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.Xml.Linq;
 
@@ -235,3 +236,242 @@ public class UnoDimensional<T> : ArrayBase
         Console.WriteLine(string.Join(" ", _array));
     }
 }   
+=======
+using System;
+using System.Xml.Linq;
+
+namespace task4._1;
+
+
+public class UnoDimensional<T> : ArrayBase
+{
+    private static T[] _array;
+    private int _size, _capacity;
+
+
+    public UnoDimensional(int capacity)
+    {
+        _capacity = capacity;
+        _size = 0;
+        _array = new T[_capacity];
+    }
+
+    public UnoDimensional() : this(7) {}
+
+    public void Add(T element)
+    {
+        if (_size >= _capacity)
+        {
+            _capacity = _capacity * 2 + 1;
+            T[] newArray = new T[_capacity];
+            Array.Copy(newArray, 0, _array, 0, _size);
+            _array = newArray;
+        }
+        _array[_size++] = element;
+    }
+
+    public void Remove(int index)
+    {
+        if (index <= _size)
+        {
+            T[] newArray = new T[_capacity];
+            for (int i = 0; i < _size; i++)
+            {
+                if (i != index)
+                {
+                    newArray[i] = _array[i];
+                }
+            }
+            _size--;
+            _array = newArray;
+            Array.Resize(ref _array, _size);
+        }
+        else
+        {
+            Console.WriteLine("You can't do this operation");
+        }
+    }
+
+
+    public T[] Reverse()
+    {
+        T[] newArray = new T[_capacity];
+        newArray = _array;
+        T count = default(T);
+        try
+        {
+            for (int i = 0; i < (newArray.Length / 2); i++)
+            {
+                count = newArray[i];
+                newArray[i] = newArray[newArray.Length - i - 1];
+                newArray[newArray.Length - i - 1] = count;
+            }
+            return newArray;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{e}, \n so you can't do this operation");
+            return _array;
+        }
+    }
+
+    public void Sorting()
+    {
+        Array.Sort(_array);
+    }
+
+    public T[] Where<U>(Func<T, bool> condition)
+    {
+        T[] _arr = new T[_array.Length];
+        int count = 0;
+        for (int i = 0; i < _array.Length; i++)
+        {
+            if (condition(_arr[i]))
+            {
+                _arr[count++] = _array[i];
+            }
+        }
+        Array.Resize(ref _arr, count);
+        return _arr;
+    }
+
+    public void PrintArrayWithCondition<U> (Func<T, bool> conditionFunc)
+    {
+        for (int i = 0; i < _array.Length; i++)
+        {
+            if (conditionFunc(_array[i]))
+            {
+                Console.Write(value: _array[i] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    public int CountArrayWithCondition<U>(Func<T, bool> conditionFunc)
+    {
+        int count = 0;
+        for (int i = 0; i < _array.Length; i++)
+        {
+            if (conditionFunc(_array[i]))
+            {
+                count += 1;
+            }
+        }
+        Console.WriteLine();
+        return count;
+    }
+
+    public int CountArray<U>()
+    {
+        int count = 0;
+        for (int i = 0; i < _array.Length; i++)
+        {
+            count += 1;
+        }
+
+        return count;
+    }
+
+    public int CountArrayFromIndex<U>(int index)
+    {
+        int count = 0;
+        for (int i = index; i < _array.Length; i++)
+        {
+            count += 1;
+        }
+        return count;
+    }
+
+    public T FindFirstElementByCondition(Func<T, bool> condition)
+    {
+        foreach (var element in _array)
+        {
+            if (condition(element))
+            {
+                return element;
+            }
+        }
+        Console.WriteLine("Таких элементов нет");
+        return (T)Convert.ChangeType(-1, typeof(T));
+    }
+
+    public bool EvenOneCondition<U>(Func<T, bool> conditionFunc)
+    {
+        for (int i = 0; i < _array.Length; i++)
+        {
+            if (conditionFunc(_array[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool AllCondition<U>(Func<T, bool> conditionFunc)
+    {
+        for (int i = 0; i < _array.Length; i++)
+        {
+            if (!conditionFunc(_array[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void ForEachAction<U>(Action<T> action)
+    {
+        for (int i = 0; i < _array.Length; i++)
+        {
+            action(_array[i]);
+        }
+    }
+
+    public T FindMin<U>(Func<T, T, int> compare) where U : IComparable<U>
+    {
+        T min = _array[0];
+        for (int i = 1; i < _array.Length; i++)
+        {
+            if (compare(_array[i], min) < 0)
+            {
+                min = _array[i];
+            }
+        }
+        return min;
+    }
+
+    public T FindMax<U>(Func<T, T, int> compare) where U : IComparable<U>
+    {
+        T max = _array[0];
+        for (int i = 1; i < _array.Length; i++)
+        {
+            if (compare(_array[i], max) > 0)
+            {
+                max = _array[i];
+            }
+        }
+        return max;
+    }
+
+    public void iterForeach(Action<T> action)
+    {
+        foreach (var item in _array)
+        {
+            if (!item.Equals(_array))
+            {
+                action(item);
+            }
+        }
+    }
+
+    public bool Contains<U>(T element)
+    {
+        return EvenOneCondition<U>((x) => x.Equals(element));
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine();
+        Console.WriteLine(string.Join(" ", _array));
+    }
+}   
+>>>>>>> 9908cc8 (done!)
